ae8cb19a552d3fb873cbee5429d2b426fcf45f1d
Adrian Kaluzinski <kaluza@gmail.com>
2022-09-04 12:51:16 +0200

    Add bootstrap data to unit test

--- src/test/java/eu/kaluzinski/sdjpaintro/BookRepositoryTest.java
+++ src/test/java/eu/kaluzinski/sdjpaintro/BookRepositoryTest.java
@@ -5,6 +5,7 @@ import eu.kaluzinski.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,19 +13,25 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
+@ComponentScan(basePackageClasses = { eu.kaluzinski.sdjpaintro.bootstrap.DataInitializer.class })
 public class BookRepositoryTest {
 
     @Autowired
     private BookRepository bookRepository;
 
     @Test
+    void shouldRunBootstrap() {
+        assertThat(bookRepository.count()).isEqualTo(2L);
+    }
+
+    @Test
     void shouldAddBooksToRepository() {
-        assertThat(bookRepository.count()).isEqualTo(0L);
+        assertThat(bookRepository.count()).isEqualTo(2L);
         final List<Book> books = Arrays.asList(
                 new Book("Some test book", "3490098", "222"),
                 new Book("Some nother test book", "34900918", "222")
         );
         bookRepository.saveAll(books);
-        assertThat(bookRepository.count()).isEqualTo(2L);
+        assertThat(bookRepository.count()).isEqualTo(4L);
     }
 }
